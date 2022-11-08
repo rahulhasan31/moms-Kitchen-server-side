e0d8eca49f6aa0b42978ae130d4164ec69ea0080
rahulhasan31 <rahulhasan666@gmail.com>
2022-11-09 04:41:43 +0600

    added

--- index.js
+++ index.js
@@ -40,6 +40,25 @@ async function run(){
             const  service= await serviceCollection.findOne(query)
             res.send(service)
         })
+
+        // review api????
+        app.get('/reviews', async (req, res)=>{
+            
+            let query= {}
+            if(req.query.email){
+                query ={
+                email: req.query.email
+                }
+            }
+            const cursor= reviewCollection.find(query)
+            const review= await cursor.toArray()
+            res.send(review)
+        })
+        app.post('/reviews', async(req, res)=>{
+            const review= req.body
+            const result= await reviewCollection.insertOne(review)
+            res.send(result)
+        })
         
     }
 
